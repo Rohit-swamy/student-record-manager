82ab64cfb51d668d963dbff0572d2fbce881b1f5
Rohit Swamy <swamyrohit07@gmail.com>
2026-07-03 19:32:00 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,117 @@
+# student record manager
+
+def menu():
+    print("1. add student record")
+    print("2. view student record")
+    print("3. search student record")
+    print("4. Total students record")
+    print("5. highest marked students record")
+    print("6. lowest marked students record")
+    print("7. exit")
+    while True:
+        choice = int(input("enter your choice :"))
+        if choice == 1:
+            add_record()
+        elif choice == 2:
+            view_record()
+        elif choice == 3:
+            search_record()
+        elif choice == 4:
+            total_students()  
+        elif choice == 5:
+            high_score()
+        elif choice == 6:
+            lowest_score()  
+        elif choice ==7:
+            break
+        else:
+            print("invalid input")
+        
+def add_record():
+      question = "y"
+      while question == "y":
+        name = input("enter the name of the student :").title()
+        age = int(input("enter the age of the student :"))
+        marks = int(input("enter the marks of the student :"))
+        with open("students.txt", "a") as f:
+            f.write(f"{name},{age},{marks}\n")
+        question = input("do you want to add another record? (y/n) :").lower()
+        while question != 'y' and question != 'n':
+            print("invalid inpput")
+            question = input("do you want to add another record? (y/n) :").lower()
+
+def view_record():
+    print("name\tage\tmarks")
+    print('-' * 30)
+    with open("students.txt","r") as f:
+        for line in f:
+            name, age, marks =line.strip().split(",")
+            print(f"{name}\t{age}\t{marks}")
+
+def search_record():
+    search_name = input("enter the name of the student :").title()
+    print("name\tage\tmarks")
+    print('-' * 30)
+    with open("students.txt","r") as f:
+        for line in f:
+            name, age, marks =line.strip().split(",")
+            if name == search_name:
+                name, age, marks =line.strip().split(",")
+                print(f"{name}\t{age}\t{marks}")
+        else:
+            print("student not found")
+
+def total_students():
+    total_students = 0
+    with open("students.txt","r") as f:
+        for line in f:
+            line.split(",")
+            count +=1
+        print("Total students :", total_students)
+
+def high_score():
+    top_student =[]
+    highest_marks = 0
+    with open("students.txt","r") as f:
+        for line in f:
+            name, age, marks = line.strip().split(",")
+            marks = int(marks)
+            if marks > highest_marks:
+                highest_marks = marks
+                top_student = []
+                top_student.append(name)
+            elif marks == highest_marks:
+                top_student.append(name)
+        if top_student== []:
+            print("no student record found")
+        else:
+            print("highest marks :", highest_marks)
+            print("top students")
+            print("----------------")
+            for student in top_student:
+                print(student)
+
+def lowest_score():
+    with open("students.txt","r") as f:
+        first_line = f.readline()
+        name, age, marks = first_line.strip().split(",")
+        marks = int(marks)
+        students = [name]
+        lowest_marks = marks
+        for line in f:
+            name, age, marks = line.strip().split(",")
+            marks = int(marks)
+            if marks < lowest_marks:
+                lowest_marks = marks
+                students = [name]
+            elif marks == lowest_marks:
+                students.append(name)
+        if students == []:
+            print("no student record found")
+        else:
+            print("lowest marks :", lowest_marks)
+            print("weak students")
+            print("----------------")
+            for student in students:
+                print(student)
+print(menu())
